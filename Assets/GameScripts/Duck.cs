80ea5283fbc44bdea1d60b651ad3e71dc8f174a8
Collin Kidder <collink@kkmfg.com>
2015-01-20 22:23:33 -0500

    Moved terrain to scenes folder and renamed it, changed duck scaling, added a test scene and some code to test ducks.

---
+++ Assets/GameScripts/Duck.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A single duck. Knows only about itself - just a script upon the duck object
+//Instantiate duck objects with DuckManager
+
+public class Duck : MonoBehaviour {
+    protected float startTime; //when was this duck spawned into being?
+    protected float lastActionTime; //last time we made a decision
+
+    //these next three can be modified to change the difficulty of this duck type.
+    public Color duckColor;
+    public float escapeTime; //how long will duck fly around before escaping?
+    public float decisionTiming; //how often this sort of duck will update its decisions
+    public float duckSpeed;
+
+    public void Start()
+    {
+        this.animation.Play("fly");
+        startTime = Time.time;
+        lastActionTime = startTime;
+        //this.renderer.material.color = duckColor;
+        //this.transform.rotation = Random.rotation;
+    }
+
+    public void Update()
+    {
+        //we have a choice to make at every update.
+        //We can continue along our current vector.
+        //We can escape (if the elapsed time is past the threshold)
+        //We can change course
+
+        if (Time.time > (startTime + escapeTime))
+        {
+            Debug.Log("Escape!");
+            //must update GUI to show that the duck got away. A signal of some sort must be sent here.
+            this.animation.Stop();
+            DestroyObject(this); //DIE! DIE! DIE!
+        }
+
+        //time to contemplate a decision now
+        if (Time.time > (lastActionTime + decisionTiming))
+        {
+            Debug.Log("Decision");
+            lastActionTime = Time.time;
+            //this.transform.rotation = Random.rotation;
+        }
+
+        //finally, if we got here then try to move in the current direction
+        //Debug.Log(transform.rotation.eulerAngles.ToString());
+
+        Vector3 movement = this.transform.rotation.eulerAngles;
+        movement.z += 270;
+        //this.transform.position += movement * Time.deltaTime * duckSpeed * 0.001f;
+    }
+}
---
+++ Assets/GameScripts/TestObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//temporarry object that allows for doing stuff as a test
+public class TestObject : MonoBehaviour {
+    public Transform duckPrefab;
+    public Transform spawnBasis;
+    public Vector3 spawnSpan;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Vector3 duckPos = spawnBasis.position;
+            duckPos.x += (float)(Random.Range(0, (float)spawnSpan.x) - (spawnSpan.x / 2.0));
+            duckPos.y += (float)(Random.Range(0, (float)spawnSpan.y) - (spawnSpan.y / 2.0));
+            duckPos.z += (float)(Random.Range(0, (float)spawnSpan.z) - (spawnSpan.z / 2.0));
+            Instantiate(duckPrefab, duckPos, Quaternion.identity);
+        }
+	}
+}
