384739d5e42adbdb92cdc8e0629a672234fb7e51
gonzoyc <gonzoyc@aol.com>
2015-01-23 21:50:36 -0500

    - Created MainMenu.unity Scene - Created Yan_TestScene.unity - Added MainMenu and MainScene scenes to build scripts. - Created GUISkins for MainMenu and Credit Screen - Created CreditScreen prefab

---
+++ Assets/GameScripts/GameCredits.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCredits : MonoBehaviour {
+
+	public GUISkin customSkin;
+	private bool showCredits = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("m")) {
+			CreditScreen();
+		}
+	
+	}
+
+	void CreditScreen(){
+		showCredits = !showCredits;
+	}
+
+	void OnGUI(){
+		
+		GUI.depth = 1;
+		
+		GUI.skin = customSkin;
+
+		if (showCredits) {
+				//GUILayout.Label("That Ducking Game");
+				GUI.BeginGroup (new Rect (Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 600));
+				//add lables here
+				int rectY = 0;
+				int addCount = 25;
+				GUI.Box (new Rect (0, rectY, 600, 600), "Credits");
+				rectY += addCount;
+				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks for playing!");
+				rectY += addCount;
+				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Developers: Brandon, Collin, and Yan");
+				rectY += addCount;
+				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Assets: Unity Store (want to add more specifics here)");
+				rectY += addCount;
+				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks to....");
+		}
+
+		GUI.EndGroup();
+		
+	}
+
+}
