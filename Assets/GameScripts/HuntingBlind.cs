458bab3dd0dc2bb318ef9933aef6bb484409f3a0
Collin Kidder <collink@kkmfg.com>
2015-01-24 23:39:02 -0500

    Shrank main scene, added hunting blind prefab, added script to colorize blinds according to difficulty

---
+++ Assets/GameScripts/HuntingBlind.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HuntingBlind : MonoBehaviour {
+    public Color tentColor;
+
+	// Use this for initialization
+	void Start () {
+        Renderer thisRenderer = this.GetComponentInChildren<Renderer>();
+        thisRenderer.material.color = tentColor;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
