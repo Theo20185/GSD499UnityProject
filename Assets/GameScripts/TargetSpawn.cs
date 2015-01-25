d2b5eab9bf70994465649b8be02581b62c428d1b
Theo20185 <Theo20185@yahoo.com>
2015-01-25 12:13:25 -0800

    EventManager Updates
    Various refactors to avoid using the term "Duck" and instead use
    "Target". Trying to avoid confusion when we have clays present in the
    game as well.
    
    EventManager can now spawn a target of the correct type with the
    parameters passed in the TargetSpawn class properties.

---
+++ Assets/GameScripts/TargetSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSpawn : MonoBehaviour 
+{
+	public enum TargetType
+	{
+		EasyDuck, HardDuck, Clay
+	}
+
+	public int targetsNeeded;
+	public TargetType targetType;
+	public int escapeTime;
+	public int targetsPerSpawn;
+	public int spawnRounds;
+	public Vector3 spawnSpan;
+}
