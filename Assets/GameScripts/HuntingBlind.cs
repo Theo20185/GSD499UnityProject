0e311debb5f8f738f0b48d00b59014b22bc89fc9
Collin Kidder <collink@kkmfg.com>
2015-01-25 11:44:40 -0500

    Implemented event start for one blind as a test.

--- Assets/GameScripts/HuntingBlind.cs
+++ Assets/GameScripts/HuntingBlind.cs
@@ -1,17 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//right now clays dont work so the clay event is instead turned into an easy duck hunt
+
 public class HuntingBlind : MonoBehaviour {
-    public Color tentColor;
+    public int tentType; //0 = clays, 1 = easy, 2 = hard
+    public EventManager eventManager;
+    public Transform spawnPoint;
+    public Transform shootPoint;
 
 	// Use this for initialization
 	void Start () {
         Renderer thisRenderer = this.GetComponentInChildren<Renderer>();
-        thisRenderer.material.color = tentColor;	
+        switch (tentType)
+        {
+            case 0:
+                thisRenderer.material.color = Color.white;
+                break;
+            case 1:
+                thisRenderer.material.color = Color.green;
+                break;
+            case 2:
+                thisRenderer.material.color = Color.red;
+                break;
+        }
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if player hit this hunting blind then start an event!
+        if (other.name == "First Person Controller")
+        {
+            if (tentType < 2)
+                eventManager.TriggerEvent(shootPoint, spawnPoint, 10, 5, true);
+            else
+                eventManager.TriggerEvent(shootPoint, spawnPoint, 10, 7, false);
+        }
+    }
 }
