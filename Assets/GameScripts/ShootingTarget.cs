72f8bab72278402dc5d552b085f98d46a4eddd4d
Collin Kidder <collink@kkmfg.com>
2015-01-28 22:16:45 -0500

    A basically completely different way to calculate duck paths. Seems to be working better but not 100% yet.

--- Assets/GameScripts/ShootingTarget.cs
+++ Assets/GameScripts/ShootingTarget.cs
@@ -14,10 +14,8 @@ public class ShootingTarget : MonoBehaviour {
 
     protected float startTime; //when was this duck spawned into being?
     protected float lastActionTime; //last time we made a decision
-    protected float inhibitTime; //keep some path modification code from executing for a bit
 
     private bool isEscaping; //sweet freedom!
-    private bool isAscending; //false would be descending
     private bool isDead; //I think you know what this means for our poor duck
     private float deadTime; //time we started to die
 
@@ -44,7 +42,7 @@ public class ShootingTarget : MonoBehaviour {
     private GameObject decoy;
 
     private Quaternion targetVector; //the travel vector we want to achieve.
-    private Vector3 originalPos; //our starting point
+    private Vector3 targetPos;
 
     public void Start()
     {
@@ -52,27 +50,26 @@ public class ShootingTarget : MonoBehaviour {
         EventManager eventMgr = (EventManager)GameObject.Find("EventManager").GetComponent<EventManager>();
         if (eventMgr.GetTargetType() == "Clays") isClay = true;
 
+        camera = GameObject.Find("Main Camera");
+        player = GameObject.Find("First Person Controller");
+
+
         if (!isClay)
         {
             sounds = GetComponents<AudioSource>();
             flapping = sounds[0];
             quack = sounds[1];
-            camera = GameObject.Find("Main Camera");
-            isAscending = true;
             isEscaping = false;
             this.animation.Play("fly");
             startTime = Time.time;
-            lastActionTime = startTime;
-            inhibitTime = Time.time - 1.0f;
-            originalPos = transform.position;
+            lastActionTime = startTime - 10f;
             Renderer thisRenderer = this.GetComponentInChildren<Renderer>();
             thisRenderer.material.color = duckColor;
-            setNewDirection(true); //immediately set direction with no interpolation
+            //transform.rotation = Quaternion.identity;
+            //SetNewHeading(true);
         }
         else
         {
-            camera = GameObject.Find("Main Camera");
-            player = GameObject.Find("First Person Controller");
             Vector3 clayPos = player.transform.position;
             startTime = Time.time;
             Vector3 forwardplayer = player.transform.forward;
@@ -84,10 +81,6 @@ public class ShootingTarget : MonoBehaviour {
 
     public void Update()
     {
-        //we have a choice to make at every update.
-        //We can continue along our current vector.
-        //We can escape (if the elapsed time is past the threshold)
-        //We can change course
         if (!isClay)
         {
             if (isDead) //do special processing if we're dead
@@ -97,9 +90,6 @@ public class ShootingTarget : MonoBehaviour {
             }
 
             float distanceToGround = 0.0f;
-
-            //interpolate the rotation vector smoothly - but quickly
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetVector, Time.deltaTime * 2);
 
             //Find out if a decoy exists - this is pretty wasteful. It would be more efficient
             //to have external code notify us of added and subtracted decoys
@@ -115,56 +105,46 @@ public class ShootingTarget : MonoBehaviour {
             //the bottom-left of the camera is (0,0); the top-right is (1,1)
             Vector3 vPoint = camera.camera.WorldToViewportPoint(transform.position);
 
-            //Make sure the duck has not gone off the left or right of the screen
-            //these are a bit before the edge of the screen so that the duck has time to turn around
-            if (vPoint.x < 0.075)
+            if (!isEscaping)
             {
-                Debug.Log("Hit left edge of screen");
-                targetOriginalPos();
-            }
 
-            if (vPoint.x > 0.925)
-            {
-                Debug.Log("Hit right edge of screen");
-                targetOriginalPos();
-            }
+                //time to contemplate a decision now so long as we're not making a break for it                
+                if (Time.time > (lastActionTime + decisionTiming))
+                {
+                    quack.Play();
+                    Debug.Log("Decision");
+                    lastActionTime = Time.time;
+                    SetNewHeading();
+                }
 
-            //distance of duck from camera
-            //Debug.Log(vPoint.z.ToString());
-            if (vPoint.z < 20f)
-            {
-                Debug.Log("Too close to player");
-                targetOriginalPos();
-                //setNewDirection();
-            }
+                //how far away from the target are we?
+                Vector3 offsetError = targetPos - transform.position;
+                Debug.Log(offsetError.sqrMagnitude.ToString());
 
-            //Also don't let duck leave top of screen unless it is escaping
-            if ((vPoint.y > 0.90 || distanceToGround > 20.0f) && !isEscaping)
-            {
-                Debug.Log("Duck is too high");
-                //isAscending = false; //got to go down!
-                //setNewDirection();
-                targetOriginalPos();
-                inhibitTime = Time.time + 1.0f;
-            }
+                //we got close enough to target
+                if (offsetError.sqrMagnitude < 5f)
+                {
+                    Debug.Log("Got to where I was going");
+                    SetNewHeading();
+                }
 
+                //Escape if it is time, we're not already escaping, and there are no decoys
+                if (Time.time > (startTime + escapeTime) && (decoy == null))
+                {
+                    Debug.Log("Escape!");
+                    isEscaping = true; //inhibits calling this code more than once
+                    //this is a pretty stiff incline upward
+                    targetVector = Quaternion.Euler(new Vector3(-50f, Random.Range(-180f, 180), 0));
+                    duckSpeed = 50; //FAST!
+                    quack.Play();
+                    //must update GUI to show that the duck got away. A signal of some sort must be sent here.
+                }
 
-            //Escape if it is time, we're not already escaping, and there are no decoys
-            if (Time.time > (startTime + escapeTime) && !isEscaping && (decoy == null))
-            {
-                Debug.Log("Escape!");
-                isEscaping = true; //inhibits calling this code more than once
-                //this is a pretty stiff incline upward
-                targetVector = Quaternion.Euler(new Vector3(-50f, Random.Range(0, 360), 0));
-                duckSpeed = 50; //FAST!
-                quack.Play();
-                //must update GUI to show that the duck got away. A signal of some sort must be sent here.
-            }
-
-            //if it was time to escape, we're not currently escaping, but there is a decoy
-            if (Time.time > (startTime + escapeTime) && !isEscaping && !(decoy == null))
-            {
-                escapeTime += 2; //give us more time
+                //if it was time to escape, we're not currently escaping, but there is a decoy
+                if (Time.time > (startTime + escapeTime) && !(decoy == null))
+                {
+                    escapeTime += 2; //give us more time
+                }
             }
 
             if (Time.time > (startTime + escapeTime + 2)) //trust me... the escape wont take long.
@@ -173,26 +153,11 @@ public class ShootingTarget : MonoBehaviour {
                 DestroyObject(this.gameObject); //DIE! DIE! DIE!
             }
 
-            //time to contemplate a decision now so long as we're not making a break for it
-            if (Time.time > (lastActionTime + decisionTiming) && !isEscaping && Time.time > inhibitTime)
-            {
-                quack.Play();
-                Debug.Log("Decision");
-                lastActionTime = Time.time;
-                if (Random.value > 0.5f)
-                {
-                    isAscending = false;
-                    Debug.Log("Going Down!");
-                }
-                else
-                {
-                    isAscending = true;
-                    Debug.Log("Headin' Up!");
-                }
-                setNewDirection();
-            }
+            //interpolate the rotation vector smoothly - but quickly
+            UpdateTargetVector();
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetVector, Time.deltaTime * 2);
 
-            //finally, if we got here then try to move in the current direction
+            //finally, move in the current direction
             transform.Translate(Vector3.forward * Time.deltaTime * duckSpeed);
         }
         else //clay shot
@@ -209,41 +174,31 @@ public class ShootingTarget : MonoBehaviour {
         }
     }
 
-    private void setNewDirection(bool immediate = false)
-    {
-        Quaternion temp = Quaternion.identity;
-        if (isAscending) targetVector = Quaternion.Euler(new Vector3(Random.Range(-40f, -10f), Random.Range(-90f, 180f), 0));
-        else
-        {
-            if (decoy == null)
-            {
-                targetVector = Quaternion.Euler(new Vector3(Random.Range(10f, 40f), Random.Range(0, 360), 0));
-            }
-            else //if we're headed downward and there is a decoy then shoot for it
-            {
-                Debug.Log("Aiming for decoy");
-                temp = transform.rotation; //remember current rotation                            
-                transform.LookAt(decoy.transform.position); //calculate a new one to look at decoy
-                targetVector = transform.rotation; //store that as our target
-                transform.rotation = temp; //restore current rotation
-            }
-        }
+    //Find view frustum and set a new random point to head to within a reasonable space in that frustum
+    //We're interested in targetting the duck anywhere on the screen and within a certain distance
+    //of the camera.
+    private void SetNewHeading(bool immediate = false)
+    { 
+        //First things first, pick a Z distance from the camera
+        float Z = Random.Range(15f, 40f); 
+        float X = Random.Range(100, camera.camera.pixelWidth - 100);
+        float Y = Random.Range((camera.camera.pixelHeight / 2f), camera.camera.pixelHeight - 100);
+        targetPos.x = X;
+        targetPos.y = Y;
+        targetPos.z = Z;
+        Debug.Log("Before: " + targetPos.ToString());
+        targetPos = camera.camera.ScreenToWorldPoint(targetPos);
+        Debug.Log("After: " + targetPos.ToString());
+
+        UpdateTargetVector();
         if (immediate) transform.rotation = targetVector;
     }
 
-    //used when we hit edge of screen. A rotation of 180 degrees in Y turns us around.
-    private void reflectDirection()
+    private void UpdateTargetVector()
     {
-        Vector3 oldDirection = transform.rotation.eulerAngles;
-        oldDirection.y += 180;
-        targetVector = Quaternion.Euler(oldDirection);
-        inhibitTime = Time.time + 1.0f;
-    }
-
-    private void targetOriginalPos()
-    {
-        Quaternion temp = transform.rotation; //remember current rotation                            
-        transform.LookAt(originalPos); //calculate a new one to look at decoy
+        Quaternion temp = transform.rotation; //remember current rotation
+        transform.rotation = Quaternion.identity;
+        transform.LookAt(targetPos); //calculate a new one to look at decoy
         targetVector = transform.rotation; //store that as our target
         transform.rotation = temp; //restore current rotation
     }
@@ -252,9 +207,7 @@ public class ShootingTarget : MonoBehaviour {
     {
         Debug.Log(other.name);
         //if the duck hit a trigger it probably means we hit the ground or lake so make sure to go up!
-        isAscending = true;
-        //and recalculate path
-        setNewDirection();
+        SetNewHeading();
     }
 
     public void Die()
