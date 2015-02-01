a7c7a7c955d11f82e82d02952b21e00b22bc2772
gonzoyc <gonzoyc@aol.com>
2015-02-01 13:46:18 -0500

    Update to Main Menu scene, added credit screen (still need text). Early work modifying dog texture.

--- Assets/GameScripts/GameCredits.cs
+++ Assets/GameScripts/GameCredits.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class GameCredits : MonoBehaviour {
 
 	public GUISkin customSkin;
+	public GameObject MainMenu;
+	public GUITexture crosshair;
 	private bool showCredits = false;
 
 	// Use this for initialization
@@ -14,14 +16,15 @@ public class GameCredits : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown ("m")) {
-			CreditScreen();
-		}
+		//if (Input.GetKeyDown ("m")) {
+			//CreditScreen();
+		//}
 	
 	}
 
-	void CreditScreen(){
+	public void CreditScreen(){
 		showCredits = !showCredits;
+		StartCoroutine(playAudio ());
 	}
 
 	void OnGUI(){
@@ -31,24 +34,42 @@ public class GameCredits : MonoBehaviour {
 		GUI.skin = customSkin;
 
 		if (showCredits) {
-				//GUILayout.Label("That Ducking Game");
-				GUI.BeginGroup (new Rect (Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 600));
-				//add lables here
-				int rectY = 0;
-				int addCount = 25;
-				GUI.Box (new Rect (0, rectY, 600, 600), "Credits");
-				rectY += addCount;
-				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks for playing!");
-				rectY += addCount;
-				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Developers: Brandon, Collin, and Yan");
-				rectY += addCount;
-				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Assets: Unity Store (want to add more specifics here)");
-				rectY += addCount;
-				GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks to....");
-		}
+			//GUILayout.Label("That Ducking Game");
+			GUI.BeginGroup (new Rect (Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 600));
+			//add lables here
+			int rectY = 0;
+			int addCount = 25;
+			GUI.Box (new Rect (0, rectY, 600, 600), "Credits");
+			rectY += addCount;
+			GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks for playing!");
+			rectY += addCount;
+			GUI.Label (new Rect (0, rectY + 50, 600, 600), "Developers: Brandon, Collin, and Yan");
+			rectY += addCount;
+			GUI.Label (new Rect (0, rectY + 50, 600, 600), "Assets: Unity Store (want to add more specifics here)");
+			rectY += addCount;
+			GUI.Label (new Rect (0, rectY + 50, 600, 600), "Thanks to....");
 
-		GUI.EndGroup();
+			if (GUI.Button (new Rect (445, 545, 150, 50), "Main Menu")) {
+				audio.Stop ();
+				showCredits = false;
+				MainMenu.GetComponent<MainMenuManager> ().ShowMainMenu ();
+			}
+
+			GUI.EndGroup();
+			
+			GUI.depth = 1;
+			
+			//cursor
+			float x = Input.mousePosition.x - (crosshair.texture.width / 2);
+			float y = Screen.height - Input.mousePosition.y - (crosshair.texture.height / 2);
+			GUI.DrawTexture (new Rect (x, y, crosshair.texture.width, crosshair.texture.height), crosshair.texture);
+		}
 		
+	}
+	
+	private IEnumerator playAudio(){
+		yield return new WaitForSeconds (.5f);
+		audio.Play ();
 	}
 
 }
--- Assets/GameScripts/MainMenuManager.cs
+++ Assets/GameScripts/MainMenuManager.cs
@@ -5,14 +5,26 @@ public class MainMenuManager : MonoBehaviour {
 
 	public GUISkin customSkin;
 	public GUITexture crosshair;
+	public GameObject gameCredits;
+	public GameObject dog;
+	public GameObject duck;
+
+	private bool showMenu = true;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		ShowMainMenu ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (showMenu) {
+
+			dog.animation.Play ("Run", PlayMode.StopAll);
+
+		}
 	
 	}
 
@@ -22,23 +34,48 @@ public class MainMenuManager : MonoBehaviour {
 
 		GUI.skin = customSkin;
 
-		//GUILayout.Label("That Ducking Game");
-		GUI.BeginGroup(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 600));
-		GUI.Box(new Rect(0, 0, 600, 600), "That\nDucking\nGame");
+		if (showMenu) {
+			//GUILayout.Label("That Ducking Game");
+			GUI.BeginGroup (new Rect (Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 600));
+			GUI.Box (new Rect (0, 0, 600, 600), "That\nDucking\nGame");
 
-		if (GUI.Button (new Rect (250, 450, 100, 50), "Start")) {
-			Application.LoadLevel("MainScene");
+			if (GUI.Button (new Rect (205, 450, 100, 50), "Start")) {
+					Application.LoadLevel ("MainScene");
+			}
+
+			if (GUI.Button (new Rect (305, 450, 100, 50), "Credits")) {
+				audio.Stop ();
+				showMenu = false;
+				dog.SetActive(false);
+				duck.SetActive(false);
+				gameCredits.GetComponent<GameCredits> ().CreditScreen ();
+			}
+
+			GUI.Label (new Rect (100, 500, 400, 100), "Top Score: 120,000");
+			GUI.EndGroup ();
+		
+			GUI.depth = 1;
+			
+			//cursor
+			float x = Input.mousePosition.x - (crosshair.texture.width / 2);
+			float y = Screen.height - Input.mousePosition.y - (crosshair.texture.height / 2);
+			GUI.DrawTexture (new Rect (x, y, crosshair.texture.width, crosshair.texture.height), crosshair.texture);
+
 		}
 
-		GUI.Label(new Rect(100, 500, 400, 100), "Top Score: 120,000");
-		GUI.EndGroup();
+	}
 
-		GUI.depth = 1;
+	public void ShowMainMenu(){
 
-		//cursor
-		float x = Input.mousePosition.x - (crosshair.texture.width / 2);
-		float y = Screen.height - Input.mousePosition.y - (crosshair.texture.height / 2);
-		GUI.DrawTexture (new Rect (x, y, crosshair.texture.width, crosshair.texture.height), crosshair.texture);
+		StartCoroutine(playAudio ());
+		showMenu = true;
+		dog.SetActive(true);
+		duck.SetActive(true);
 
+	}
+
+	private IEnumerator playAudio(){
+		yield return new WaitForSeconds (.5f);
+		audio.Play ();
 	}
 }
