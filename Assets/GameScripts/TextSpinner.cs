8fc3cfbefaa4e1ed08814c82f581a81a32a9f38f
Collin Kidder <collink@kkmfg.com>
2015-02-14 16:48:28 -0500

    Added spinning text above each tent so that they're easier to find even if the player gets a bit turned around.

---
+++ Assets/GameScripts/TextSpinner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextSpinner : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    private TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+        textMesh = GetComponent<TextMesh>();
+        transform.eulerAngles = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Time.deltaTime * rotationSpeed;
+        transform.RotateAround(textMesh.renderer.bounds.center, new Vector3(0,1,0), y);
+	}
+}
